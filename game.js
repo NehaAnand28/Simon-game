52634e5dda25397eff06fe6206d56ffe3b96b715
Neha Anand <neha280103@gmail.com>
2022-02-19 19:26:26 +0530

    ✨ added flash animation and sounds

--- game.js
+++ game.js
@@ -8,17 +8,27 @@ var gamePattern = [];
 
 var buttonColours = ["red", "green", "yellow", "blue"];
 
-function debug(input){
-    console.log(input);
+function debug(input) {
+  console.log(input);
 }
 
-function nextSequence(){
-    //generates a random number between 0 and 3 (4 numbers)
-    var randomNumber = Math.floor(Math.random() * 4);
-    debug(randomNumber);
-    var randomChosenColour = buttonColours[randomNumber];
-    debug(randomChosenColour);
-    gamePattern.push(randomChosenColour);
-    debug(gamePattern);
+function nextSequence() {
+  //generates a random number between 0 and 3 (4 numbers)
+  var randomNumber = Math.floor(Math.random() * 4);
+  debug(randomNumber);
+  var randomChosenColour = buttonColours[randomNumber];
+  debug(randomChosenColour);
+  gamePattern.push(randomChosenColour);
+  debug(gamePattern);
+  
+  //using jQuery to choose the div with same id as randomChosenColour
+  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
 
+  //to play audio in javascript
+  var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+  audio.play();
 }
+
+
+
+nextSequence();
