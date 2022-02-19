3f975ca8f0a131e449346e3c03a72a234074ffc4
Neha Anand <neha280103@gmail.com>
2022-02-19 12:46:41 +0530

    ✨ created game pattern

--- game.js
+++ game.js
@@ -4,3 +4,21 @@
 //test if jQuery is linked to html correctly by logging 👇 to console
 // console.log($("h1"));
 
+var gamePattern = [];
+
+var buttonColours = ["red", "green", "yellow", "blue"];
+
+function debug(input){
+    console.log(input);
+}
+
+function nextSequence(){
+    //generates a random number between 0 and 3 (4 numbers)
+    var randomNumber = Math.floor(Math.random() * 4);
+    debug(randomNumber);
+    var randomChosenColour = buttonColours[randomNumber];
+    debug(randomChosenColour);
+    gamePattern.push(randomChosenColour);
+    debug(gamePattern);
+
+}
